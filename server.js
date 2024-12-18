ecd548a5d389af84e79675762cbfcd4aff75fb5f
Zalmatt <matizalazar2001@gmail.com>
2024-12-17 22:05:51 -0300

    New-Vercel.json

--- server.js
+++ server.js
@@ -1,45 +1,41 @@
 const express = require('express');
-const cors = require('cors'); // Importa CORS para manejo de solicitudes entre dominios
-const { admin } = require('./utils/firebase'); // Importar Firebase desde utils/firebase.js
+const cors = require('cors');
+const { admin } = require('./utils/firebase');
 const googleRoute = require('./routes/googleRoute');
 
-
-require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
+require('dotenv').config();
 
 const app = express();
 
 // Configurar CORS
 app.use(cors({
-  origin: 'http://localhost:3000', // Dirección del frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  credentials: true, // Permitir envío de cookies y headers de autenticación
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 }));
 
-app.use(express.json()); // Middleware para parsear JSON
+app.use(express.json());
 
-// Importa las rutas
+// Importar rutas
 const productRoute = require('./routes/productRoute');
 const superuserRoute = require('./routes/superuserRoute');
 const userRoute = require('./routes/userRoute');
 const tipoProductoRoute = require('./routes/tipoProductoRoute');
 const authRoute = require('./routes/authRoute');
-const favoriteRoutes = require('./routes/favoritesRoute'); // Importar las rutas de favoritos
-const notificationRoutes = require('./routes/notificationRoute'); // Importa las rutas de notificaciones
+const favoriteRoutes = require('./routes/favoritesRoute');
+const notificationRoutes = require('./routes/notificationRoute');
 const ventasRoute = require('./routes/ventasRoute');
 
-// Configura las rutas
+// Configurar rutas
 app.use('/api/productos', productRoute);
 app.use('/api/superusers', superuserRoute);
 app.use('/api/users', userRoute);
 app.use('/api/tipos_product', tipoProductoRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/google', googleRoute);
-app.use('/api/favorites', favoriteRoutes); // Rutas de favoritos
-app.use('/api/notificaciones', notificationRoutes); // Registra las rutas bajo el prefijo /api/notificaciones
+app.use('/api/favorites', favoriteRoutes);
+app.use('/api/notificaciones', notificationRoutes);
 app.use('/api/ventas', ventasRoute);
 
-// Inicia el servidor
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+// Exportar como handler para Vercel
+module.exports = app;
